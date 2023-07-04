66f820c96d0abc8c7fbf64bf117691c602f16d56
UnicusCoder <mahiuddin.ahmed.neel@gmail.com>
2023-07-04 19:12:13 +0600

    age-added

--- demo.py
+++ demo.py
@@ -1,5 +1,8 @@
 fname = input("Enter your first name: ")
 lname = input("Enter your last name: ")
 your_name = fname + " " + lname
+your_age = input("Enter your age: ")
+
 print(your_name)
+print(your_age)
 
